2ef72b287c57b41b1609a8a18c3edbb84a5c83cf
Andrew Szucs <andrew0szucs@gmail.com>
2017-02-23 15:23:17 +0100

    fix(app): rename app_finl to app

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app_finl = Flask(__name__)
+app = Flask(__name__)
 
-@app_finl.route('/')
+@app.route('/')
 def hello():
     return "Hello World!"
 
 if __name__ == '__main__':
-    app_finl.run()
+    app.run()
